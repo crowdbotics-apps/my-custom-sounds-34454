b39ab3ee62ec5b98d08d0a7a91abdb54573e711d
Crowdbotics <team@crowdbotics.com>
2022-03-27 03:33:48 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/my_custom_sounds_34454/MainActivity.java
@@ -0,0 +1,15 @@
+package com.my_custom_sounds_34454;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "my_custom_sounds_34454";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "my_custom_sounds_34454",
+  url: "https://my_custom_sounds_34454.botics.co",
+  api: "https://my_custom_sounds_34454.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
